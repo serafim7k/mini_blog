45b837490bbbcb872c2e94176c70edcfa4c15b16
serafim7k <mycomputer10j12@gmail.com>
2023-05-15 18:36:02 +0300

    web server

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request,redirect
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -15,13 +15,40 @@ class Article(db.Model):
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"Article: {self.id}, {self.username}, {self.title}"
+        return f"{self.text}"
+
+    def show_type(self):
+        return 'Our type Article'
 
 
 with app.app_context():
     db.create_all()
 
 
+# app2 = Flask(__name__)
+# app2.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project2.db'
+# db2 = SQLAlchemy(app2)
+
+
+# class User(db2.Model):
+#     id = db2.Column(db2.Integer, primary_key=True)
+#     name = db2.Column(db2.String, unique=True, nullable=False)
+#     email = db2.Column(db2.String(100), nullable=False)
+#     password = db2.Column(db2.Text, nullable=False)
+#     date = db2.Column(db2.DateTime, default=datetime.utcnow)
+
+#     def __repr__(self):
+#         return f"{self.email}"
+
+#     def show_type(self):
+#         return 'Our type User'
+
+
+# with app2.app_context():
+#     db2.create_all()
+
+
+# @app2.route('/')
 @app.route('/')
 @app.route('/home')
 def index():
@@ -39,10 +66,79 @@ def create_article():
         username = request.form['username']
         title = request.form['title']
         text = request.form['text']
-        print(username, title, type(text))
-        return redirect('/home')
+
+        article = Article(username=username, title=title, text=text)
+        # print(article.show_type())
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return render_template("500.html")
+
     else:
         return render_template("create-article.html")
+
+
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     if request.method == "POST":
+#         name = request.form['name']
+#         email = request.form['email']
+#         password = request.form['password']
+
+#         article = User(name=name, email=email, password=password)
+#         try:
+#             db2.session.add(article)
+#             db2.session.commit()
+#             return redirect('/')
+#         except:
+#             return render_template("500.html")
+
+#     else:
+#         return render_template("login.html")
+
+
+@app.route('/posts')
+def posts():
+    article = Article.query.order_by(Article.date.desc()).all()
+    return render_template('posts.html', articles=article)
+
+
+@app.route('/posts/<int:id>')
+def posts_detail(id):
+    article = Article.query.get(id)
+    print(article)
+    return render_template('posts_detail.html', article=article)
+
+
+@app.route('/posts/<int:id>/delete')
+def posts_delete(id):
+    article = Article.query.get_or_404(id)
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return render_template("500.html")
+
+
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def posts_update(id):
+    article = Article.query.get_or_404(id)
+    if request.method == 'POST':
+        article.username = request.form.get('username')
+        article.title = request.form.get('title')
+        article.text = request.form.get('text')
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return render_template("500.html")
+    else:
+        return render_template('post_edit.html', article=article)
+
 
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
---
+++ static/js/main.js
@@ -0,0 +1,3 @@
+$('.authorization__form-input').on('click', function() {
+    $('.authorization__form-input').addClass('authorization__form-input--touch');
+})
